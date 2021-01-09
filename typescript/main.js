133049848220ebafa862c30e5822fde2807aa85d
jaedie <jamesleehw@gmail.com>
2021-01-09 19:04:53 +0900

    test2

--- typescript/main.js
+++ typescript/main.js
@@ -1,4 +1,17 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 exports.__esModule = true;
 var message = "Hello James, Welcome Back";
 console.log(message);
@@ -37,3 +50,72 @@ function hasName(obj) {
 if (hasName(myVariable)) {
     console.log(myVariable.name);
 }
+var a;
+a = 10;
+a = true;
+var b = 20;
+var multiType;
+multiType = 20;
+multiType = true;
+var anyType;
+anyType = 20;
+anyType = true;
+function add(num1, num2) {
+    if (num2 === void 0) { num2 = 10; }
+    if (num2)
+        return num1 + num2;
+    else
+        return num1;
+}
+add(5, 10);
+add(5);
+function fullName(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+;
+var p = {
+    firstName: 'Bruce',
+    lastName: 'Wayne'
+};
+fullName(p);
+var Employee = /** @class */ (function () {
+    function Employee(name) {
+        this.employeeName = name;
+    }
+    Employee.prototype.greet = function () {
+        console.log("Good Morning " + this.employeeName);
+    };
+    return Employee;
+}());
+var emp1 = new Employee("Vishwas");
+var emp2 = new Employee("James");
+console.log(emp1.employeeName);
+emp1.greet();
+console.log(emp2.employeeName);
+emp2.greet();
+// class wecode {
+//     studentName: string;
+//     constructor(name:string) {
+//         this.studentName = name;
+//     }
+//     farewell() {
+//         console.log(`Byebye ${this.studentName}`)
+//     }
+// }
+// let stu1 = new wecode(`lol`);
+// console.log(stu1.studentName);
+// stu1.farewell();
+var Manager = /** @class */ (function (_super) {
+    __extends(Manager, _super);
+    function Manager(managerName) {
+        return _super.call(this, managerName) || this;
+    }
+    Manager.prototype.delegateWork = function () {
+        console.log("Manager delegating tasks");
+    };
+    return Manager;
+}(Employee));
+var m1 = new Manager("Bruce");
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
--- typescript/main.ts
+++ typescript/main.ts
@@ -45,6 +45,101 @@ if (hasName(myVariable)) {
     console.log(myVariable.name);
 }
 
+let a: number | boolean;
+a = 10;
+a = true;
+
+let b = 20;
+
+let multiType: number | boolean;
+multiType = 20;
+multiType = true;
+
+let anyType: any;
+anyType = 20;
+anyType = true;
+
+function add(num1: number, num2: number = 10): number {
+    if (num2)
+        return num1 + num2;
+    else
+        return num1;
+}
+add(5, 10);
+add(5);
+
+interface Person {
+    firstName: string;
+    lastName?: string;
+}
+
+function fullName (person: Person) {
+    console.log(`${person.firstName} ${person.lastName}`);
+};
+
+let p = {
+    firstName: 'Bruce',
+    lastName: 'Wayne',
+};
+
+fullName(p);
+
+class Employee {
+    protected employeeName: string;
+    constructor(name: string) {
+        this.employeeName = name;
+    }
+
+    greet() {
+        console.log(`Good Morning ${this.employeeName}`)
+    }
+}
+
+let emp1 = new Employee(`Vishwas`);
+let emp2 = new Employee(`James`);
+console.log(emp1.employeeName);
+emp1.greet();
+console.log(emp2.employeeName);
+emp2.greet();
+
+// class wecode {
+//     studentName: string;
+//     constructor(name:string) {
+//         this.studentName = name;
+//     }
+
+//     farewell() {
+//         console.log(`Byebye ${this.studentName}`)
+//     }
+// }
+
+// let stu1 = new wecode(`lol`);
+// console.log(stu1.studentName);
+// stu1.farewell();
+
+class Manager extends Employee {
+    constructor(managerName: string) {
+        super(managerName);
+    }
+    delegateWork() {
+        console.log(`Manager delegating tasks`);
+    }
+}
+
+let m1 = new Manager(`Bruce`);
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
+
+
+
+
+
+
+
+
+
+
 
 
 
